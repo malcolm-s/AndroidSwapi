55be366c7ca482ea3a01611c1f46a6a09ed04bb1
Malcolm Stone <malcolm.stone@geeks.ltd.uk>
2016-06-30 12:48:52 +0100

    add recycler view to main activity

--- app/src/main/java/com/malcolmstone/androidswapi/MainActivity.java
+++ app/src/main/java/com/malcolmstone/androidswapi/MainActivity.java
@@ -1,13 +1,27 @@
 package com.malcolmstone.androidswapi;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.malcolmstone.androidswapi.databinding.MainActivityBinding;
+import com.malcolmstone.androidswapi.databinding.MainActivtyBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MainActivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.main_activity);
+
+        binding.personList.setLayoutManager(new LinearLayoutManager(this));
+        binding.personList.setHasFixedSize(true);
     }
 }
